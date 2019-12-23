0e3a4b7e53902e9e3216267366af0674af3993ec
Denys Boiko <denys.boiko@gmail.com>
2019-12-24 00:35:36 +0200

    debug

--- alerts_map/settings.py
+++ alerts_map/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+# DEBUG = os.environ['DEBUG']
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
