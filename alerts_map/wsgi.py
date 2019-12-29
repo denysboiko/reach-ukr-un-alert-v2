2d11258aa59edf978a54013ccb13623c0ce7fc08
Denys Boiko <denys.boiko@gmail.com>
2019-12-29 22:58:05 +0200

    fix wsgi

--- alerts_map/wsgi.py
+++ alerts_map/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for alerts_map project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alerts_map.settings")
-
-application = get_wsgi_application()
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '':
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
